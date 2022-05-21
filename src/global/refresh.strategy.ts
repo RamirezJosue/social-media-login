64984c5a8e4169d27c30415a10dfe6702034472d
RamirezJosue <ramirezjry17@gmail.com>
2022-05-21 18:06:18 -0500

    update

---
+++ src/global/refresh.strategy.ts
@@ -0,0 +1,38 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+
+@Injectable()
+export class RefreshStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
+    constructor() {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: 'fsehfjksefsebjkfsebfbjksefjsebfbjkse',
+            passReqToCallback: true
+        })
+    }
+
+    validate(req: Request, payload: JwtPayload): JwtPayloadWithRt {
+        const refreshToken = req
+          ?.get('authorization')
+          ?.replace('Bearer', '')
+          .trim();
+    
+        if (!refreshToken) throw new ForbiddenException('Refresh token malformed');
+    
+        return {
+          ...payload,
+          refreshToken,
+        };
+      }
+}
+
+export type JwtPayload = {
+    email: string;
+    sub: number;
+  };
+  
+
+export type JwtPayloadWithRt = JwtPayload & { refreshToken: string };
